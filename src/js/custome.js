866e17dde2889a6d726c47a814dfe0a030253c1f
sigitpratm <sigitama19@gmail.com>
2022-03-21 18:04:39 +0700

    update adjust tablet 1024

--- src/js/custome.js
+++
@@ -1,9 +0,0 @@
-/**
- * ============================================================================
- * Author - Sigit Pratama
- * Author github - https://github.com/sigitpratm
- * Dev core - Mochamad Yudi Sobari
- * Dev Core Github - https://github.com/mochamadyudi
- * @copyright - Emkalab
- * ============================================================================
- */
