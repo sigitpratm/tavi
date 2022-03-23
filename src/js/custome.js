f6b5dad7e4117c64a6e5b8847c0ab4614a45a910
sigitpratm <sigitama19@gmail.com>
2022-03-23 17:10:37 +0700

    update quiz

--- src/js/custome.js
+++ src/js/custome.js
@@ -1,3 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    
+
 });
+
+// $("#next-session3").click(function () {
+//     let x = $(".chk-money-cond > input:checkbox:checked");
+//     if (x.length < 3) {
+//         alert("Anda harus memilih 3 pilihan");
+//     } else {
+//         $("#session-3").hide();
+//         $("#session-4").fadeIn(500);
+//         $(window).scrollTop(0);
+//     }
+// });
